b5c61a05b5ded4119ca099b20b9e5288f68dffaf
kwren <kevin.wren@viasat.com>
2017-12-10 15:43:16 -0800

    Added puzzle type, and a harder puzzle. For loop for iterating over puzzles.

--- suduku.go
+++ suduku.go
@@ -5,9 +5,11 @@ import (
 	"github.com/Wrenky/sudoKu/solve"
 )
 
+type Puzzle [][]uint
+
 func main() {
 	fmt.Println("Starting!")
-	startingPuzzle := [][]uint{
+	easyPuzzle := Puzzle{
 		{0, 0, 0, 6, 0, 4, 7, 0, 0},
 		{7, 0, 6, 0, 0, 0, 0, 0, 9},
 		{0, 0, 0, 0, 0, 5, 0, 8, 0},
@@ -21,21 +23,37 @@ func main() {
 		{0, 0, 2, 3, 0, 1, 0, 0, 0},
 	}
 
-	out := solve.Display(startingPuzzle)
-	fmt.Println("Starting Puzzle:")
-	fmt.Println("-------------------------------")
-	fmt.Println(out)
-	fmt.Println("-------------------------------")
+	hardPuzzle := Puzzle{
+		{0, 0, 0, 0, 1, 0, 9, 0, 0},
+		{9, 0, 0, 7, 0, 2, 3, 0, 0},
+		{0, 5, 0, 8, 0, 9, 0, 0, 0},
 
-	res, err := solve.SolvePuzzle(startingPuzzle)
-	if err != nil {
-		fmt.Println("Some unknown error during solving.")
-		fmt.Println(err)
-	} else {
-		fmt.Println("Solved:")
-		output := solve.Display(res)
-		fmt.Println("-------------------------------")
-		fmt.Println(output)
-		fmt.Println("-------------------------------")
+		{0, 0, 5, 0, 3, 8, 0, 0, 0},
+		{1, 0, 0, 0, 0, 0, 0, 0, 9},
+		{0, 9, 0, 1, 2, 0, 6, 0, 0},
+
+		{0, 0, 0, 2, 0, 7, 0, 3, 0},
+		{0, 0, 6, 3, 0, 4, 0, 0, 2},
+		{0, 0, 8, 0, 6, 0, 0, 0, 0},
+	}
+
+	puzzleList := []Puzzle{easyPuzzle, hardPuzzle}
+	puzzleList = []Puzzle{hardPuzzle}
+
+	fmt.Println("----------------------------------------------------------")
+	for index, puzzle := range puzzleList {
+		fmt.Printf("Puzzle #%d initial state:\n", index)
+		out := solve.Display(puzzle)
+		fmt.Println(out)
+		res, err := solve.SolvePuzzle(puzzle)
+		if err != nil {
+			fmt.Println("Some unknown error during solving.")
+			fmt.Println(err)
+		} else {
+			fmt.Println("Solved:")
+			output := solve.Display(res)
+			fmt.Println(output)
+		}
+		fmt.Println("----------------------------------------------------------")
 	}
 }
