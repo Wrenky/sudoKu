5fc356605457039326d99a1f41f38b08a58385bd
kwren <kevin.wren@viasat.com>
2017-12-12 23:39:55 -0800

    Adding carl's puzzle to solve list

--- suduku.go
+++ suduku.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/Wrenky/sudoKu/solve"
+	"reflect"
 )
 
 type Puzzle [][]uint
@@ -13,11 +14,9 @@ func main() {
 		{0, 0, 0, 6, 0, 4, 7, 0, 0},
 		{7, 0, 6, 0, 0, 0, 0, 0, 9},
 		{0, 0, 0, 0, 0, 5, 0, 8, 0},
-
 		{0, 7, 0, 0, 2, 0, 0, 9, 3},
 		{8, 0, 0, 0, 0, 0, 0, 0, 5},
 		{4, 3, 0, 0, 1, 0, 0, 7, 0},
-
 		{0, 5, 0, 2, 0, 0, 0, 0, 0},
 		{3, 0, 0, 0, 0, 0, 2, 0, 8},
 		{0, 0, 2, 3, 0, 1, 0, 0, 0},
@@ -37,14 +36,24 @@ func main() {
 		{0, 0, 8, 0, 6, 0, 0, 0, 0},
 	}
 
-	puzzleList := []Puzzle{easyPuzzle, hardPuzzle}
-	puzzleList = []Puzzle{hardPuzzle}
+	carlPuzzle := Puzzle{
+		{7, 3, 4, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 3, 9, 2, 0},
+		{0, 0, 0, 0, 8, 5, 0, 0, 6},
+		{0, 0, 0, 0, 0, 0, 1, 3, 8},
+		{0, 0, 0, 4, 7, 2, 0, 0, 0},
+		{5, 9, 6, 0, 0, 0, 0, 0, 0},
+		{3, 0, 0, 9, 6, 0, 0, 0, 0},
+		{0, 1, 2, 8, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 7, 5, 4},
+	}
+
+	puzzleList := []Puzzle{easyPuzzle, hardPuzzle, carlPuzzle}
 
 	fmt.Println("----------------------------------------------------------")
 	for index, puzzle := range puzzleList {
-		fmt.Printf("Puzzle #%d initial state:\n", index)
 		out := solve.Display(puzzle)
-		fmt.Println(out)
+		fmt.Printf("Puzzle #%d initial state:\n%s\n", index, out)
 		res, err := solve.SolvePuzzle(puzzle)
 		if err != nil {
 			fmt.Println("Some unknown error during solving.")
@@ -56,6 +65,10 @@ func main() {
 		}
 		fmt.Println("----------------------------------------------------------")
 		hintedPuzzle, row, col, err := solve.Hint(puzzle)
+		fmt.Println("Type of err: ", reflect.TypeOf(err))
+		if pe, ok := err.(*solve.PuzzleError); ok {
+			fmt.Printf("Puzzled! At (%d,%d): --%s--", pe.Row, pe.Col, pe)
+		}
 		fmt.Printf("Getting a hint, post hint puzzle(%d,%d):\n", row, col)
 		output := solve.Display(hintedPuzzle)
 		fmt.Println(output)
